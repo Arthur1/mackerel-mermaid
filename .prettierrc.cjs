3e003284eb458430dfd0a7814c725da086dfc4d8
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2023-10-21 14:14:38 +0900

    eslint & prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: "avoid",
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
